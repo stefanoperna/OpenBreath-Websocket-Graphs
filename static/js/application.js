c24086a601702b65f920467af11b3105a7d939af
Simone Iannucci <simoiannucci@gmail.com>
2020-04-25 22:06:35 +0200

    Update application.js

--- static/js/application.js
+++ static/js/application.js
@@ -1,8 +1,5 @@
-const SAMPLE_NUM = 300;
 var lineChart;
-var count = 0;
-var limSup = 0.5;
-var limInf = -0.3;
+
 
 $(document).ready(function(){
     //connect to the socket server.
@@ -25,25 +22,21 @@ function createGraph1() {
 
     var limitSupArray = [];
     var limitInfArray = [];
-    var baseLine = [];
-    var baseLineLabel = [];
     var step;
-    
-    for (step = 0; step < SAMPLE_NUM; step++) {
-        limitSupArray.push(limSup);
-        baseLine.push(0);
-        baseLineLabel.push('00:00:00');
-        limitInfArray.push(limInf);
+    var range =0;
+    for (step = 0; step < 300; step++) {
+        limitSupArray.push(0.5);
+        limitInfArray.push(-0.5);
     }
 
     var ctx = document.getElementById("lineChart").getContext("2d");
     lineChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: baseLineLabel,
+        labels: [],
         datasets: [{
             label: 'SIN',
-            data: baseLine,
+            data: [],
             pointRadius: 0,
             backgroundColor: [
                 'rgba(25, 99, 132, 0.2)'
@@ -106,23 +99,15 @@ function createGraph1() {
 }
 
 function addData(chart, label, data) {
-    const NULL_NUM = 5;
-    if (count < SAMPLE_NUM - NULL_NUM){
-        chart.data.labels[count] = label;
-        chart.data.datasets[0].data[count] = data;
-        var i;
-        for (i = 1; i < NULL_NUM; i++) {
-            chart.data.datasets[0].data[count+i] = null;
-        } 
-    }else{
-        chart.data.labels[count] = label;
-        chart.data.datasets[0].data[count] = data;
-    }
+ 
+      
+    chart.data.labels.push(label);
+    chart.data.datasets[0].data.push(data);
 
-    if (data < limInf || data > limSup){
+    if (Math.abs(data)>0.5){
         chart.data.datasets[0].backgroundColor[0] = 'rgba(255, 99, 132, 0.5)';
         chart.data.datasets[0].borderColor[0] = 'rgba(255, 99, 99, 1)';
-    }
+}
     else{
         chart.data.datasets[0].backgroundColor[0] = 'rgba(25, 99, 132, 0.2)';
         chart.data.datasets[0].borderColor[0] = 'rgba(1, 1, 255, 1)';
@@ -131,11 +116,6 @@ function addData(chart, label, data) {
     if (chart.data.datasets[0].data.length === 300){
     chart.data.labels.shift();
     chart.data.datasets[0].data.shift();
-    }
-    
-    count = count + 1;
-    if (count == SAMPLE_NUM){
-        count = 0;
     }
     chart.update();
  
